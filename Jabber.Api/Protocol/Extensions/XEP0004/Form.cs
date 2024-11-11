df935f86db1c6085f61f292f568f6c0afc14fe5c
Nathan Ferreira <14365254+nathan130200@users.noreply.github.com>
2024-11-11 00:24:21 -0300

    Add buildspec to publish nuget package.
    - Also split Item from dataform in separated file.

--- Jabber.Api/Protocol/Extensions/XEP0004/Form.cs
+++ Jabber.Api/Protocol/Extensions/XEP0004/Form.cs
@@ -43,30 +43,3 @@ public class Form : Element
         }
     }
 }
-
-[XmppTag("item", Namespaces.DataForms)]
-public class Item : Element
-{
-    public Item() : base("item", Namespaces.DataForms)
-    {
-
-    }
-
-    public IEnumerable<Field> Fields
-    {
-        get => Children<Field>();
-        set
-        {
-            Children<Field>().Remove();
-
-            if (value?.Any() == true)
-            {
-                foreach (var item in value)
-                    AddChild(item);
-            }
-        }
-    }
-
-    public void AddField(Field field)
-        => AddChild(field);
-}
---
+++ Jabber.Api/Protocol/Extensions/XEP0004/Item.cs
@@ -0,0 +1,31 @@
+﻿using Jabber.Attributes;
+using Jabber.Dom;
+
+namespace Jabber.Protocol.Extensions.XEP0004;
+
+[XmppTag("item", Namespaces.DataForms)]
+public class Item : Element
+{
+    public Item() : base("item", Namespaces.DataForms)
+    {
+
+    }
+
+    public IEnumerable<Field> Fields
+    {
+        get => Children<Field>();
+        set
+        {
+            Children<Field>().Remove();
+
+            if (value?.Any() == true)
+            {
+                foreach (var item in value)
+                    AddChild(item);
+            }
+        }
+    }
+
+    public void AddField(Field field)
+        => AddChild(field);
+}
